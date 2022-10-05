15d5173ad50d02bf57ad10cf6dbe1f16451f3630
Artem Solovyov <solovevartem892@gmail.com>
2022-10-05 19:01:56 +0300

    Paginator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+import Paginator from "./Paginator/Paginator";
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [totalPages, setTotalPages] = useState();
+  const [page, setPage] = useState(1);
+  const [pageExample, setPageExample] = useState(3);
+
+  const onClickPage = (page) => {
+    setPage(page)
+  }
+
+  const onClickPageExample = (page) => {
+    setPageExample(page)
+  }
+  useEffect(()=>{
+    fetch(`https://reqres.in/api/users?page=${page}`)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          setItems(data.data)
+          setTotalPages(data.total_pages)
+        });
+  }, [page])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {items && items.map(item=>{
+        return (
+            <div key={item.id}>
+              <div>
+                <img src={item.avatar} alt="avatar"/>
+              </div>
+              <div>{item.id}.{item.first_name} {item.last_name}</div>
+              <div>{item.email}</div>
+            </div>
+        )
+      })}
+      <br/>
+      <Paginator pagesCount={totalPages} currentPage={page}  onPageChanged={onClickPage} portionSize={2} />
+      <hr/>
+      <h1>Example</h1>
+      <hr/>
+      <Paginator pagesCount={30} currentPage={pageExample}  onPageChanged={onClickPageExample} portionSize={6} />
     </div>
   );
 }
